98dc97186cfc1224affb7ca9e6527dd900614932
Lumennes <6032158+Lumennes@users.noreply.github.com>
2023-07-06 21:00:34 +0500

    5

---
+++ test-project/Assets/Scripts/Editor/UnityBuilderAction/BuildScript.cs
@@ -0,0 +1,293 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+using UnityEditor.AddressableAssets.Build;
+using UnityEditor.AddressableAssets.Settings;
+
+namespace UnityBuilderAction
+{
+    public static class BuildScript
+    {
+        private static readonly string Eol = Environment.NewLine;
+
+        private static readonly string[] Secrets =
+            {"androidKeystorePass", "androidKeyaliasName", "androidKeyaliasPass"};
+
+        public static void Build()
+        {
+            // Gather values from args
+            Dictionary<string, string> options = GetValidatedOptions();
+
+            // Set version for this build
+            PlayerSettings.bundleVersion = options["buildVersion"];
+            PlayerSettings.macOS.buildNumber = options["buildVersion"];
+            PlayerSettings.Android.bundleVersionCode = int.Parse(options["androidVersionCode"]);
+
+            // Apply build target
+            var buildTarget = (BuildTarget)Enum.Parse(typeof(BuildTarget), options["buildTarget"]);
+            switch (buildTarget)
+            {
+                case BuildTarget.Android:
+                    {
+                        EditorUserBuildSettings.buildAppBundle = options["customBuildPath"].EndsWith(".aab");
+                        if (options.TryGetValue("androidKeystoreName", out string keystoreName) &&
+                            !string.IsNullOrEmpty(keystoreName))
+                        {
+                            PlayerSettings.Android.useCustomKeystore = true;
+                            PlayerSettings.Android.keystoreName = keystoreName;
+                        }
+                        if (options.TryGetValue("androidKeystorePass", out string keystorePass) &&
+                            !string.IsNullOrEmpty(keystorePass))
+                            PlayerSettings.Android.keystorePass = keystorePass;
+                        if (options.TryGetValue("androidKeyaliasName", out string keyaliasName) &&
+                            !string.IsNullOrEmpty(keyaliasName))
+                            PlayerSettings.Android.keyaliasName = keyaliasName;
+                        if (options.TryGetValue("androidKeyaliasPass", out string keyaliasPass) &&
+                            !string.IsNullOrEmpty(keyaliasPass))
+                            PlayerSettings.Android.keyaliasPass = keyaliasPass;
+                        if (options.TryGetValue("androidTargetSdkVersion", out string androidTargetSdkVersion) &&
+                            !string.IsNullOrEmpty(androidTargetSdkVersion))
+                        {
+                            var targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
+                            try
+                            {
+                                targetSdkVersion =
+                                    (AndroidSdkVersions)Enum.Parse(typeof(AndroidSdkVersions), androidTargetSdkVersion);
+                            }
+                            catch
+                            {
+                                UnityEngine.Debug.Log("Failed to parse androidTargetSdkVersion! Fallback to AndroidApiLevelAuto");
+                            }
+
+                            PlayerSettings.Android.targetSdkVersion = targetSdkVersion;
+                        }
+
+                        break;
+                    }
+                //case BuildTarget.StandaloneOSX:
+                //    PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.Mono2x);
+                //    break;
+            }
+
+            // Custom build
+            Build(buildTarget, options["customBuildPath"]);
+        }
+
+        private static Dictionary<string, string> GetValidatedOptions()
+        {
+            ParseCommandLineArguments(out Dictionary<string, string> validatedOptions);
+
+            if (!validatedOptions.TryGetValue("projectPath", out string _))
+            {
+                Console.WriteLine("Missing argument -projectPath");
+                EditorApplication.Exit(110);
+            }
+
+            if (!validatedOptions.TryGetValue("buildTarget", out string buildTarget))
+            {
+                Console.WriteLine("Missing argument -buildTarget");
+                EditorApplication.Exit(120);
+            }
+
+            if (!Enum.IsDefined(typeof(BuildTarget), buildTarget ?? string.Empty))
+            {
+                EditorApplication.Exit(121);
+            }
+
+            if (!validatedOptions.TryGetValue("customBuildPath", out string _))
+            {
+                Console.WriteLine("Missing argument -customBuildPath");
+                EditorApplication.Exit(130);
+            }
+
+            const string defaultCustomBuildName = "TestBuild";
+            if (!validatedOptions.TryGetValue("customBuildName", out string customBuildName))
+            {
+                Console.WriteLine($"Missing argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+                validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            }
+            else if (customBuildName == "")
+            {
+                Console.WriteLine($"Invalid argument -customBuildName, defaulting to {defaultCustomBuildName}.");
+                validatedOptions.Add("customBuildName", defaultCustomBuildName);
+            }
+
+            return validatedOptions;
+        }
+
+        private static void ParseCommandLineArguments(out Dictionary<string, string> providedArguments)
+        {
+            providedArguments = new Dictionary<string, string>();
+            string[] args = Environment.GetCommandLineArgs();
+
+            Console.WriteLine(
+                $"{Eol}" +
+                $"###########################{Eol}" +
+                $"#    Parsing settings     #{Eol}" +
+                $"###########################{Eol}" +
+                $"{Eol}"
+            );
+
+            // Extract flags with optional values
+            for (int current = 0, next = 1; current < args.Length; current++, next++)
+            {
+                // Parse flag
+                bool isFlag = args[current].StartsWith("-");
+                if (!isFlag) continue;
+                string flag = args[current].TrimStart('-');
+
+                // Parse optional value
+                bool flagHasValue = next < args.Length && !args[next].StartsWith("-");
+                string value = flagHasValue ? args[next].TrimStart('-') : "";
+                bool secret = Secrets.Contains(flag);
+                string displayValue = secret ? "*HIDDEN*" : "\"" + value + "\"";
+
+                // Assign
+                Console.WriteLine($"Found flag \"{flag}\" with value {displayValue}.");
+                providedArguments.Add(flag, value);
+            }
+        }
+
+        private static void Build(BuildTarget buildTarget, string filePath)
+        {
+            bool contentBuildSucceeded = BuildAddressables();
+
+            if (contentBuildSucceeded)
+            {
+                string[] scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(s => s.path).ToArray();
+                var buildPlayerOptions = new BuildPlayerOptions
+                {
+                    scenes = scenes,
+                    target = buildTarget,
+                    //                targetGroup = BuildPipeline.GetBuildTargetGroup(buildTarget),
+                    locationPathName = filePath,
+                    //                options = UnityEditor.BuildOptions.Development
+                    options = BuildOptions.CompressWithLz4HC
+                };
+
+                BuildSummary buildSummary = BuildPipeline.BuildPlayer(buildPlayerOptions).summary;
+                ReportSummary(buildSummary);
+                ExitWithResult(buildSummary.result);
+            }
+        }
+
+        private static void ReportSummary(BuildSummary summary)
+        {
+            Console.WriteLine(
+                $"{Eol}" +
+                $"###########################{Eol}" +
+                $"#      Build results      #{Eol}" +
+                $"###########################{Eol}" +
+                $"{Eol}" +
+                $"Duration: {summary.totalTime.ToString()}{Eol}" +
+                $"Warnings: {summary.totalWarnings.ToString()}{Eol}" +
+                $"Errors: {summary.totalErrors.ToString()}{Eol}" +
+                $"Size: {summary.totalSize.ToString()} bytes{Eol}" +
+                $"{Eol}"
+            );
+        }
+
+        private static void ExitWithResult(BuildResult result)
+        {
+            switch (result)
+            {
+                case BuildResult.Succeeded:
+                    Console.WriteLine("Build succeeded!");
+                    EditorApplication.Exit(0);
+                    break;
+                case BuildResult.Failed:
+                    Console.WriteLine("Build failed!");
+                    EditorApplication.Exit(101);
+                    break;
+                case BuildResult.Cancelled:
+                    Console.WriteLine("Build cancelled!");
+                    EditorApplication.Exit(102);
+                    break;
+                case BuildResult.Unknown:
+                default:
+                    Console.WriteLine("Build result is unknown!");
+                    EditorApplication.Exit(103);
+                    break;
+            }
+        }
+
+        public static string build_script
+            = "Assets/AddressableAssetsData/DataBuilders/BuildScriptPackedMode.asset";
+        public static string settings_asset
+            = "Assets/AddressableAssetsData/AddressableAssetSettings.asset";
+        public static string profile_name = "Default";
+        private static AddressableAssetSettings settings;
+
+        static void getSettingsObject(string settingsAsset)
+        {
+            // This step is optional, you can also use the default settings:
+            //settings = AddressableAssetSettingsDefaultObject.Settings;
+
+            settings
+                = AssetDatabase.LoadAssetAtPath<ScriptableObject>(settingsAsset)
+                    as AddressableAssetSettings;
+
+            if (settings == null)
+                Debug.LogError($"{settingsAsset} couldn't be found or isn't " +
+                               $"a settings object.");
+        }
+
+        static void setProfile(string profile)
+        {
+            string profileId = settings.profileSettings.GetProfileId(profile);
+            if (String.IsNullOrEmpty(profileId))
+                Debug.LogWarning($"Couldn't find a profile named, {profile}, " +
+                                 $"using current profile instead.");
+            else
+                settings.activeProfileId = profileId;
+        }
+
+        static void setBuilder(IDataBuilder builder)
+        {
+            int index = settings.DataBuilders.IndexOf((ScriptableObject)builder);
+
+            if (index > 0)
+                settings.ActivePlayerDataBuilderIndex = index;
+            else
+                Debug.LogWarning($"{builder} must be added to the " +
+                                 $"DataBuilders list before it can be made " +
+                                 $"active. Using last run builder instead.");
+        }
+
+        static bool buildAddressableContent()
+        {
+            AddressableAssetSettings
+                .BuildPlayerContent(out AddressablesPlayerBuildResult result);
+            bool success = string.IsNullOrEmpty(result.Error);
+
+            if (!success)
+            {
+                Debug.LogError("Addressables build error encountered: " + result.Error);
+            }
+            return success;
+        }
+
+        // [MenuItem("Window/Asset Management/Addressables/Build Addressables only")]
+        public static bool BuildAddressables()
+        {
+            getSettingsObject(settings_asset);
+            setProfile(profile_name);
+            IDataBuilder builderScript
+              = AssetDatabase.LoadAssetAtPath<ScriptableObject>(build_script) as IDataBuilder;
+
+            if (builderScript == null)
+            {
+                Debug.LogError(build_script + " couldn't be found or isn't a build script.");
+                return false;
+            }
+
+            setBuilder(builderScript);
+
+            return buildAddressableContent();
+        }        
+    }
+}
